47199b1a2f77284863385adbb5f662b3975b42c3
Gabriel Ramos <ramosbrasil25@gmail.com>
2021-01-19 19:04:04 -0200

    inicio do front

--- frontendcrud/src/App.js
+++ frontendcrud/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import ListEmployeeComponent from './components/ListEmployeeComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"> 
-        <h1>Teste</h1>
-      </header>
+     <ListEmployeeComponent/>
     </div>
   );
 }
