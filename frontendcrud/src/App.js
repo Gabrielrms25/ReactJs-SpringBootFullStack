6db5218d1b89e2a394cbbe13311dbc7af4817aad
Gabriel Ramos <ramosbrasil25@gmail.com>
2021-01-20 19:10:47 -0200

    utilizando react-router-dom

--- frontendcrud/src/App.js
+++ frontendcrud/src/App.js
@@ -5,14 +5,25 @@ import HeaderComponent from './components/HeaderComponent';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 
 function App() {
-  return (
-    <div>
-      <HeaderComponent/>
-        <div className="App">
-           <ListEmployeeComponent/>
-        </div>
-        <FooterComponent/>
-    </div>
+
+return (
+  <div>
+    <Router>
+       <div className="App">
+          <HeaderComponent/>
+
+             <div className="App">
+               <Switch> http://localhost:3000/employees
+                  <Route path = "/" component={ListEmployeeComponent}></Route>
+                  <Route path = "/employees" component={ListEmployeeComponent}></Route>
+                  <ListEmployeeComponent/>
+              </Switch> 
+             </div>
+
+           <FooterComponent/>
+       </div>
+    </Router>
+  </div>
   );
 }
 
