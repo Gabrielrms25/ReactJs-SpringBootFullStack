f8be60fd1e9252be986d518078dcdc6a43c38ae5
Gabriel Ramos <ramosbrasil25@gmail.com>
2021-01-22 15:33:53 -0200

    implementando método POST no back end

---
+++ springboot-backendCRUD/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -0,0 +1,38 @@
+package net.javaguides.springboot.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.repository.EmployeeRepository;
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/")
+
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	// get all employees
+	
+	@GetMapping("employees")
+	public List<Employee> getAllEmployees(){
+		return employeeRepository.findAll();
+		 
+	}
+	
+	// create employee rest api
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
+	}
+	
+}
