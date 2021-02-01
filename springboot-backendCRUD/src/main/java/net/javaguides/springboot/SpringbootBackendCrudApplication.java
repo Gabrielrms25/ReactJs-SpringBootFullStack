c78994565c8c5a83126c35af33104d8ebdf161f3
Gabriel Ramos <ramosbrasil25@gmail.com>
2021-02-01 18:46:43 -0200

    metodo delete implementado com sucesso(back-end)

--- springboot-backendCRUD/src/main/java/net/javaguides/springboot/SpringbootBackendCrudApplication.java
+++ springboot-backendCRUD/src/main/java/net/javaguides/springboot/SpringbootBackendCrudApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 
-
 @SpringBootApplication
 @ComponentScan(basePackages = {"net.javaguides.springboot.controller"})
 public class SpringbootBackendCrudApplication {
--- springboot-backendCRUD/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ springboot-backendCRUD/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package net.javaguides.springboot.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,10 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 import net.javaguides.springboot.exception.ResourceNotFoundException;
 import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
+
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
-
 public class EmployeeController {
 	
 	@Autowired
@@ -27,7 +30,7 @@ public class EmployeeController {
 	
 	// get all employees
 	
-	@GetMapping("employees")
+	@GetMapping("/employees")
 	public List<Employee> getAllEmployees(){
 		return employeeRepository.findAll();
 		 
@@ -61,6 +64,18 @@ public class EmployeeController {
 		Employee updateEmployee = employeeRepository.save(employee);
 		return ResponseEntity.ok(updateEmployee);
 		
+	}
+	
+	//delete employee rest api
+	@DeleteMapping("/employees/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id){
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Delete successfull"+ id));
+		
+		employeeRepository.delete(employee);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
 	}
 	
 }
