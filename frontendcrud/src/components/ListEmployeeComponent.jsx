002e1a5df0278fdc7fa57dceccff7ff362909b66
Gabriel Ramos <ramosbrasil25@gmail.com>
2021-02-01 19:07:00 -0200

    metodo delete implemetado com sucesso(front e back end)

--- frontendcrud/src/components/ListEmployeeComponent.jsx
+++ frontendcrud/src/components/ListEmployeeComponent.jsx
@@ -10,6 +10,13 @@ class ListEmployeeComponent extends Component {
         }
         this.addEmployee = this.addEmployee.bind(this);
         this.editEmployee = this.editEmployee.bind(this);
+        this.deleteEmployee = this.deleteEmployee.bind(this);
+    }
+
+    deleteEmployee(id){
+        EmployeeService.deleteEmployee(id).then(res => {
+            this.setState({employees:this.state.employees.filter(employee => employee.id !== id)});
+        });
     }
 
     
@@ -57,6 +64,7 @@ class ListEmployeeComponent extends Component {
                                         <td>{employee.emailId}</td>   
                                         <td>
                                             <button onClick = {() =>this.editEmployee(employee.id)} className="btn btn-info" >Update</button>
+                                            <button style={{marginLeft:"10px"}} onClick = {() =>this.deleteEmployee(employee.id)} className="btn btn-danger" >Delete</button>
                                         </td>
                                     </tr>       
                                 )
--- frontendcrud/src/services/EmployeeService.js
+++ frontendcrud/src/services/EmployeeService.js
@@ -20,6 +20,11 @@ class EmployeeService {
         return axios.put(EMPLOYEE_API_BASE_URL + '/' + employeeId, employee);
     }
 
+    deleteEmployee(employeeId){
+        return axios.delete(EMPLOYEE_API_BASE_URL + '/' + employeeId);
+              
+    }
+
 }
 
 
